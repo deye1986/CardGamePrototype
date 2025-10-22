96712ecfee94cb1b030276558a98eca8667c1605
Dave Ikin <davidikin6@gmail.com>
2025-10-22 10:26:10 +0100

    Tidy script, added card dealer opponent mechanics and UI, - looking to combine score counting & fix draw card method next.

--- Assets/Scripts/DeckManager.cs
+++ Assets/Scripts/DeckManager.cs
@@ -10,9 +10,21 @@ public class DeckManager : MonoBehaviour, IPointerClickHandler
 
     public GameObject cardPrefab;
     public List<Card> deck;
+
     public GameObject playerArea;
+    public GameObject dealerArea;
+
     public TMP_Text playerScoreText;
+    public TMP_Text dealerScoreText;
+
+
     public List<Card> playerCards;
+    public List<Card> dealerCards;
+
+
+    private int dealerScore; // ? already a value in the playerscoretext G.O.'s
+
+    private bool isDealersTurn = false;
 
     void Start()
     {
@@ -70,17 +82,26 @@ public class DeckManager : MonoBehaviour, IPointerClickHandler
 
     public void DrawCard()
     {
-        Debug.Log("Drawing card");
+        Debug.Log("Drawing cards for player..");
         Card playerCard = Instantiate(deck[Random.Range(0, deck.Count)], new Vector3(0, 0, 0), Quaternion.identity);
         playerCard.transform.SetParent(playerArea.transform, false);
         playerCards.Add(playerCard);
 
+        Debug.Log("cards for dealer ..");
+        Card dealerCard = Instantiate(deck[Random.Range(0, deck.Count)], new Vector3(0, 0, 0), Quaternion.identity);
+        dealerCard.transform.SetParent(dealerArea.transform, false);
+        dealerCards.Add(dealerCard);
+
         AddToPlayerScore(playerCard.value);
         playerScoreText.text = $"{playerScore}";
-        HasPlayerBust();
+        HasPlayerBust(); 
+
+        AddToDealerScore(dealerCard.value);
+        dealerScoreText.text = $"{dealerScore}";
+        // has dealer bust function or has bust universal function, that checkss both participants scores
     }
 
-    private int AddToPlayerScore(int value)
+    private int AddToPlayerScore(int value) // combine score methods 
     {
         if (value > 10)
         {
@@ -91,6 +112,19 @@ public class DeckManager : MonoBehaviour, IPointerClickHandler
         
         playerScore += value;
         return playerScore;
+    }
+
+    private int AddToDealerScore(int value) // combine score metods
+    {
+        if (value > 10)
+        {
+            value = 10;
+        }
+
+        // ace logic
+        
+        dealerScore += value;
+        return dealerScore;
     }
 
     public void HasPlayerBust()
@@ -104,5 +138,7 @@ public class DeckManager : MonoBehaviour, IPointerClickHandler
     public void GameOver(string text)
     {
         Debug.Log(text);
+        Debug.Log("dealer turn...");
+
     }
 }
